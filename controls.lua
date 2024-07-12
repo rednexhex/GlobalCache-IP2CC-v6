134ff97c771326bf563d30e28f10797f6483477f
rednexhex <rednexhex@gmail.com>
2024-07-11 20:48:54 -0500

    First commit

---
+++ controls.lua
@@ -0,0 +1,86 @@
+
+  table.insert(ctrls, {
+    Name = "address",
+    ControlType = "Text",
+    DefaultValue = 'Enter and IP Address' 
+  })
+  table.insert(ctrls, {
+    Name = "port",
+    ControlType = "Knob",
+    ControlUnit = "Integer",
+    Min = 1,
+    Max = 65535,
+    DefaultValue = 4998
+  })
+
+  table.insert(ctrls, {
+    Name = "rly1",
+    ControlType = "Button",
+    ButtonType = "Trigger",
+    UserPin = true,
+    PinStyle = "Input",
+    Count = 1
+  })
+  table.insert(ctrls, {
+    Name = "rlytime1",
+    ControlType = "Knob",
+    ControlUnit = "Integer",
+    Min = 0,
+    Max = 60,
+    DefaultValue = 0
+  })
+  table.insert(ctrls, {
+    Name = "rlyled1",
+    ControlType = "Indicator",
+    IndicatorType = "LED",
+    UserPin = false,
+    Count = 1
+  })
+
+  table.insert(ctrls, {
+    Name = "rly2",
+    ControlType = "Button",
+    ButtonType = "Trigger",
+    UserPin = true,
+    PinStyle = "Input",
+    Count = 1
+  })
+  table.insert(ctrls, {
+    Name = "rlytime2",
+    ControlType = "Knob",
+    ControlUnit = "Integer",
+    Min = 0,
+    Max = 60,
+    DefaultValue = 0
+  })
+  table.insert(ctrls, {
+    Name = "rlyled2",
+    ControlType = "Indicator",
+    IndicatorType = "LED",
+    UserPin = false,
+    Count = 1
+  })
+
+  table.insert(ctrls, {
+    Name = "rly3",
+    ControlType = "Button",
+    ButtonType = "Trigger",
+    UserPin = true,
+    PinStyle = "Input",
+    Count = 1
+  })
+  table.insert(ctrls, {
+    Name = "rlytime3",
+    ControlType = "Knob",
+    ControlUnit = "Integer",
+    Min = 0,
+    Max = 60,
+    DefaultValue = 0
+  })
+  table.insert(ctrls, {
+    Name = "rlyled3",
+    ControlType = "Indicator",
+    IndicatorType = "LED",
+    UserPin = false,
+    Count = 1
+  })
---
+++ info.lua
@@ -0,0 +1,13 @@
+
+PluginInfo = {
+  Name = "Matty~GlobalCache~IP2CC-v6",
+  Version = "6.0",
+  BuildVersion = "6.5.0.0",
+  Id = "083a1d2c-7473-4748-a79e-c31602a6fb77",
+  Author = "Matt Gililland",
+  Description = "Control of Relay Box via TCP/IP"
+}
+
+Signature = "--[[ #encode "./images/MATTY.png" ]]"
+
+logo = "--[[ #encode "./images/gcscaled.png" ]]"
